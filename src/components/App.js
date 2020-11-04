405cf775798a4d7e2613900cb38087e99345add4
Samuel STPL <samuelstpierrelariviere@gmail.com>
2020-11-04 14:49:22 -0500

    MVP

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import useInterval from "../hooks/use-interval.hook";
 import GlobalStyles from "./GlobalStyles";
 import Home from "./Home";
 import Game from "./Game";
+import { GameContext } from "./GameContext";
 
-function App(props) {
+function App() {
+  const { numCookies, setNumCookies, calculateCookiesPerSecond } = useContext(
+    GameContext
+  );
+
+  useInterval(() => {
+    const numOfGeneratedCookies = calculateCookiesPerSecond;
+
+    setNumCookies(numCookies + numOfGeneratedCookies);
+  }, 1000);
+
+  React.useEffect(() => {
+    document.title = `${numCookies} cookies - Cookie Clicker Workshop`;
+
+    return () => {
+      document.title = "Cookie Clicker Workshop";
+    };
+  }, [numCookies]);
+
+  // Store cookies in Local storage
+  useEffect(() => {
+    localStorage.setItem("CookiesNumber", numCookies);
+  }, [numCookies]);
+
   return (
     <>
       <GlobalStyles />
